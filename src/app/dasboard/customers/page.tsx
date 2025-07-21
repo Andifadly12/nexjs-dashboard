e9826ba1940100facb14f38ad69c389062a6f541
Andifadly12 <andifadly2004@gmail.com>
2025-07-21 22:19:16 +0800

    commit: slecing for cutomers

--- src/app/dasboard/customers/page.tsx
+++ src/app/dasboard/customers/page.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
 export default function page() {
-  return <div>customers page</div>;
+  return (
+    <div>
+      <form action="">
+        <label className="text-black block mb-2 text-sm font-medium ">
+          Name Customers :
+        </label>
+        <input
+          type="text"
+          placeholder="Masukkan name"
+          className="border border-gray-700 p-2 rounded-md w-full text-black "
+        />
+        <label className="text-black block mb-2 text-sm font-medium ">
+          Email :
+        </label>
+        <input
+          type="email"
+          placeholder="Masukkan name"
+          className="border border-gray-700 p-2 rounded-md w-full text-black "
+        />
+        <label className="text-black block mb-2 text-sm font-medium ">
+          Revenue
+        </label>
+        <input
+          type="text"
+          placeholder="Masukkan name"
+          className="border border-gray-700 p-2 rounded-md w-full text-black "
+        />
+        <label
+          htmlFor="image"
+          className="text-black block mb-2 text-sm font-medium"
+        >
+          Upload Image:
+        </label>
+        <input
+          type="file"
+          className="text-black border border-gray-700 text-center p-2 "
+        />
+      </form>
+    </div>
+  );
 }
